6e36b572f88c4a01af119fb784dca89fd83bf115
DEV.JHUNRIZ <93062821+Jhunriz@users.noreply.github.com>
2026-01-15 21:10:17 +0800

    add vue-router and create Home component with routing

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>dasdasdsad</h1>
+</template>
+
+<script>
+export default {
+    // name below was the component name to know what the component was named
+    // Note the Home file name and route should be not the same
+    name: 'HomePage'
+}
+</script>
