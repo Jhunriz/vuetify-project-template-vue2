f2c64b471b77fe80b0823d6a273634614b010058
DEV.JHUNRIZ <93062821+Jhunriz@users.noreply.github.com>
2026-01-15 20:50:24 +0800

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+Vue.use(Vuex) 
+
+export default new Vuex.Store({
+    // where the actual state origin 
+    state: {
+        count: 0
+    },
+    mutations: {
+        // actual function where incrementation of data.
+        increment(state) {
+            state.count++
+        },
+        decrement(state) {
+            state.count--
+        }
+    },
+    actions: {
+        increment(commit) {
+            commit('increment')
+        },
+        decrement(commit) {
+            commit(commit)
+        }
+    }
+})
